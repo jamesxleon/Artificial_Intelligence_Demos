4cc37c0a904ec9594c4abf39feba53a2369a2583
jamesxleon <90779073+jamesxleon@users.noreply.github.com>
2023-06-20 17:11:58 -0500

    Cannibals code from Luger's F_W_G_C book code

--- D1/Cannibal.pl
+++ D1/Cannibal.pl
@@ -1,12 +1,51 @@
 :- [adts]
 
 go(Start, Goal) :-
-    empty_queue(Empty_open),
-    state_record(Start, nil, 0, 0, Start, Start_state),
-    add_to_queue(Start_state, Empty_open, Open),
-    empty_set(Closed),
-    path(Open, Closed, Goal).
+    empty_queue(Empty_been_queue),
+    add_to_queue(Start, Empty_been_queue, Been_queue)
+    path(Start, Goal, Been_queue).
 
-path(Goal, Goal, Been, Queue) :- 
+path(Goal, Goal, Been_queue) :-
     write('Solution path is: '), nl,
-    printsolution(Goal, Been, Queue).
+    print_queue(Been_queue).
+
+path(State, Goal, Been_queue) :-
+    move(State, Next_state),
+    not(member_queue(Next_state, Been_queue)),
+    add_to_queue(Next_state, Been_queue, New_been_queue),
+    path(Next_state, Goal, New_been_queue), !.
+    
+move(state(X, X, Cannibals), state(Y, Y, Cannibals)) :-
+    opp(X, Y), not(unsafe(state(Y, Y, Cannibals))),
+    writelist(['try two missionaries cross the river', Y, Y, Cannibals])
+    
+move(state(X, Missionaries, X), state(Y, Missionaries, Y)) :-
+    opp(X, Y), not(unsafe(state(Y, Missionaries, Y))),
+    writelist(['try two cannibals cross the river', Y, Missionaries, Y])
+
+move(state(X, Missionaries, Cannibals), State(Y, Missionaries, Cannibals)) :-
+    opp(X,Y), not(unsafe(state(Y, Missionaries, Cannibals))),
+    writelist(['try one missionary amd one cannibal cross the river', Y, Missionaries, Cannibals])
+
+move(state(X, Missionaries, Cannibals), state(X, Missionaries, Cannibals)) :- 
+    writelist(['      BACKTRACK from:',X,Missionaries,Cannibals]), fail.
+
+unsafe(state(X, Missionaries, Cannibals)) :-
+    Missionaries > 0, Cannibals > Missionaries,
+    opp(X, OtherSide), 
+    OtherCannibals is 3 - Cannibals,
+    OtherMissionaries is 3 - Missionaries,
+    OtherMissionaries > 0, OtherCannibals > OtherMissionaries.
+
+writelist([]) :- nl.
+writelist([H|T]) :- write(H), tab(1), writelist(T).
+
+opp(e,w).
+opp(w,e).
+
+print_queue(Q) :-
+    empty_queue(Q).
+print_queue(Q) :-
+    remove_from_queue(E, Q, Rest),
+    print_queue(Rest),
+    write(E), nl.
